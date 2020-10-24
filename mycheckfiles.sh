e990630316456cc8e65b3e2deebe8c7ab63f5267
AWS ParallelCluster user <ec2-user@ip-10-0-4-34.ap-southeast-1.compute.internal>
2020-10-24 05:06:10 +0000

    Commit changes for lab 7 step 1

---
+++ mycheckfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ls
+ls session01
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | xargs ls -hl
+find mountains -name "firings.mda" | wc -l
